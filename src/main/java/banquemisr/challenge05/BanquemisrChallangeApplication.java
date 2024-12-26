34b210f89cfdf80024259fd02b0b5480325c1aeb
Youssif Tawfik <y.tawfik-c@futurework.com.sa>
2024-12-26 17:33:45 +0200

    Initial commit

---
+++ src/main/java/banquemisr/challenge05/BanquemisrChallangeApplication.java
@@ -0,0 +1,13 @@
+package banquemisr.challenge05;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BanquemisrChallangeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BanquemisrChallangeApplication.class, args);
+	}
+
+}
